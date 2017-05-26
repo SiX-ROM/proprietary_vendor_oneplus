9994e87d1c55c56ac3123d3772048d7871703555
joshuous <joshuous@gmail.com>
2017-05-26 14:16:03 +0000

    op3: Import 32-bit libopbaselib.so camera blob

--- oneplus3/oneplus3-vendor.mk
+++ oneplus3/oneplus3-vendor.mk
@@ -99,6 +99,7 @@ PRODUCT_COPY_FILES += \
     vendor/oneplus/oneplus3/proprietary/etc/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf \
     vendor/oneplus/oneplus3/proprietary/lib/TC_Bayer_Converter_v6_core_opt.bin:system/lib/TC_Bayer_Converter_v6_core_opt.bin \
     vendor/oneplus/oneplus3/proprietary/lib64/libfilter-sdk.so:system/lib64/libfilter-sdk.so \
+    vendor/oneplus/oneplus3/proprietary/lib/libopbaselib.so:system/lib/libopbaselib.so \
     vendor/oneplus/oneplus3/proprietary/lib64/libopbaselib.so:system/lib64/libopbaselib.so \
     vendor/oneplus/oneplus3/proprietary/lib64/libopcamera.so:system/lib64/libopcamera.so \
     vendor/oneplus/oneplus3/proprietary/lib64/libopcameralib.so:system/lib64/libopcameralib.so \
